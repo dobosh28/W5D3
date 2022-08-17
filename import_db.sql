cf781a0131eff5654b46db76a8bdefaba209c057
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-17 16:45:05 -0400

    create questions database

--- import_db.sql
+++ import_db.sql
@@ -1,8 +1,19 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
 DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE questions(
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+
+CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
@@ -11,26 +22,16 @@ CREATE TABLE questions(
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users(
-  id INTEGER PRIMARY KEY,
-  fname TEXT NOT NULL,
-  lname TEXT NOT NULL
-);
-
-DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows(
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    -- account for asker and follower
+
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS replies;
 
 CREATE TABLE replies(
     id INTEGER PRIMARY KEY,
@@ -44,7 +45,6 @@ CREATE TABLE replies(
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE question_likes(
     id INTEGER PRIMARY KEY,
