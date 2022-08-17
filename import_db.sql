1e403883a310d4049c3f8bbcd7aa221b6cedd308
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-17 16:22:42 -0400

    start testing

--- import_db.sql
+++ import_db.sql
@@ -1,12 +1,17 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions(
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS question_follows;
-DROP TABLE IF EXISTS replies;
-DROP TABLE IF EXISTS question_likes;
-
 
 CREATE TABLE users(
   id INTEGER PRIMARY KEY,
@@ -14,23 +19,18 @@ CREATE TABLE users(
   lname TEXT NOT NULL
 );
 
-CREATE TABLE questions(
-  id INTEGER PRIMARY KEY,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  user_id INTEGER NOT NULL
-
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
+DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows(
-    -- id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     -- account for asker and follower
-    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+DROP TABLE IF EXISTS replies;
 
 CREATE TABLE replies(
     id INTEGER PRIMARY KEY,
@@ -39,17 +39,19 @@ CREATE TABLE replies(
     user_id INTEGER NOT NULL,
     body TEXT NOT NULL,
 
-    FOREIGN KEY (subject_q) REFERENCES questions(id)
-    FOREIGN KEY (parent_reply) REFERENCES replies(id)
+    FOREIGN KEY (subject_q) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply) REFERENCES replies(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE question_likes(
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     subject_q INTEGER NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (subject_q) REFERENCES questions(id)
 );
 
